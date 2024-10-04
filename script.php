fc18801892ecc70b0b31ebfbc635343ddd0bfc54
Akshat2512 <akshat.2016603@gmail.com>
2024-10-04 21:33:16 +0530

    Adding vercel.json file

--- script.php
+++ script.php
@@ -4,6 +4,11 @@ if(isset($_POST['url1']))
 $url = $_POST['url1'];
 $i = $_POST['val'];
 
+
+if (!is_dir('captures')) {
+    mkdir('captures', 0777, true)
+}
+
 $img = file_get_contents($url);
 $file = fopen("captures/IMG$i.jpg", "w");
 fwrite($file, $img);
