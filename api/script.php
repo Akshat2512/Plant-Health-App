43530885ec21b30cc63445c90d21d7e382cfb311
Akshat2512 <akshat.2016603@gmail.com>
2024-10-05 05:19:11 +0530

    deploy

--- api/script.php
+++ api/script.php
@@ -3,27 +3,7 @@ header('Content-Type: application/json');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-
-$url = $_POST['url1'];
-$i = $_POST['val'];
-
-
-if (!is_dir('captures')) {
-    mkdir('captures', 0777, true)
-}
-
-$img = file_get_contents($url);
-$file = fopen("captures/IMG$i.jpg", "w");
-fwrite($file, $img);
-fclose($file);
-
-$command = `python "Python_Backend/plant_disease_detection.py" $i`;
-
-$output = escapeshellarg($command);
-
-echo $output;
-
-die;
+echo "Hello World"
 
 }
 ?>
