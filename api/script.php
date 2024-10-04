061099ec0076e5ad4789e2998956fcd6ef56e5f8
Akshat2512 <akshat.2016603@gmail.com>
2024-10-05 01:03:03 +0530

    changes in vercel.json

---
+++ api/script.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['url1']))
+{
+$url = $_POST['url1'];
+$i = $_POST['val'];
+
+
+if (!is_dir('captures')) {
+    mkdir('captures', 0777, true)
+}
+
+$img = file_get_contents($url);
+$file = fopen("captures/IMG$i.jpg", "w");
+fwrite($file, $img);
+fclose($file);
+
+$command = `python "Python_Backend/plant_disease_detection.py" $i`;
+
+$output = escapeshellarg($command);
+
+echo $output;
+
+die;
+}
+die;
+?>
