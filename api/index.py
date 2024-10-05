c387038bc3d0264b790e18ebeca610bf727f93d1
Akshat2512 <akshat.2016603@gmail.com>
2024-10-06 03:10:11 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -5,6 +5,10 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-@app.route('/greet', methods=['POST'])
+@app.route('/')
+def index():
+    return "hello"
+
+@app.route('/greet', methods=['GET'])
 def index():
     return "hello_world"
