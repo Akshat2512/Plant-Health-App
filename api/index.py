6d894966e098f0b51cd1d020dcaec1ce601038f1
Akshat2512 <akshat.2016603@gmail.com>
2024-10-06 03:23:26 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
 # import subprocess,json
 from flask import Flask, render_template, request, redirect
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
