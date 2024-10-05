46b77a7b6e9ff3e17717f60d3fd64ac1e71e656e
Akshat2512 <akshat.2016603@gmail.com>
2024-10-05 11:54:19 +0530

    commit

--- api/index.php
+++ api/index.php
@@ -4,19 +4,16 @@
 if(isset($_POST['url1']))
 {
 $url = $_POST['url1'];
-$url_1 =  $_POST['url2'];
 $i = $_POST['val'];
 
-$cap_Dir = $url_1;
+$cap_Dir = 'tmp/captures';
 
 if (!is_dir($cap_Dir)) {
     mkdir($url_1, 0777, true);
 }
 
 $files = scandir($url_1);
-echo "<pre>";
-print_r($files);
-echo "</pre>";
+
 $img = file_get_contents($url);
 $file = fopen("$cap_Dir/IMG$i.jpg", "w");
 fwrite($file, $img);
