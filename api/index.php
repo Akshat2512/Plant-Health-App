b64e7d00871668f21634ff0b7e7e0ed9981246ae
Akshat2512 <akshat.2016603@gmail.com>
2024-10-05 11:18:32 +0530

    commit

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ $cap_Dir = $url_1;
 // if (!is_dir($cap_Dir)) {
 //     mkdir('\captures', 0777, true);
 // }
-echo $_SERVER['DOCUMENT_ROOT'];
+echo dirname(__DIR__, 2);
 $img = file_get_contents($url);
 $file = fopen("$cap_Dir/IMG$i.jpg", "w");
 fwrite($file, $img);
