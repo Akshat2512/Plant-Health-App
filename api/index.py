7ca2cb8edfffffe222a4c8dadf88a0bbb327fa86
Akshat2512 <akshat.2016603@gmail.com>
2024-10-06 03:52:29 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -1,14 +1,16 @@
 # import subprocess,json
-from flask import Flask, render_template, request, redirect
-from flask_cors import CORS
+# from flask import Flask, render_template, request, redirect
+# from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route('/')
-def index():
-    return "hello"
+# @app.route('/')
+# def index():
+#     return "hello"
 
-@app.route('/greet', methods=['GET'])
-def  greet():
-    return "hello_world"
+# @app.route('/greet', methods=['GET'])
+# def  greet():
+#     return "hello_world"
+
+return "Hello World"
