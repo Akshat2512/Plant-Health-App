24b84515bb2afbe79239431ff837d6c52e0bd8ca
Akshat2512 <akshat.2016603@gmail.com>
2024-10-05 09:32:57 +0530

    commit

--- api/script.php
+++ api/script.php
@@ -1,26 +1,27 @@
 <?php
-if(isset($_POST['url1']))
-{
-$url = $_POST['url1'];
-$i = $_POST['val'];
+echo "Hello world";
+// if(isset($_POST['url1']))
+// {
+// $url = $_POST['url1'];
+// $i = $_POST['val'];
 
 
-if (!is_dir('captures')) {
-    mkdir('captures', 0777, true)
-}
+// if (!is_dir('captures')) {
+//     mkdir('captures', 0777, true)
+// }
 
-$img = file_get_contents($url);
-$file = fopen("captures/IMG$i.jpg", "w");
-fwrite($file, $img);
-fclose($file);
+// $img = file_get_contents($url);
+// $file = fopen("captures/IMG$i.jpg", "w");
+// fwrite($file, $img);
+// fclose($file);
 
-$command = `python "Python_Backend/plant_disease_detection.py" $i`;
+// $command = `python "Python_Backend/plant_disease_detection.py" $i`;
 
-$output = escapeshellarg($command);
+// $output = escapeshellarg($command);
 
-echo $output;
+// echo $output;
 
-die;
-}
-die;
+// die;
+// }
+// die;
 ?>
