d5d15b433644727fca6cd51c447e58332bc23bfd
Akshat2512 <akshat.2016603@gmail.com>
2024-10-06 07:15:56 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -6,9 +6,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def greet():
