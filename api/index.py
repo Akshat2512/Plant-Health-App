de0adc82da59a028732bc5c140bd25191517135c
Akshat2512 <akshat.2016603@gmail.com>
2024-10-06 14:31:32 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 import subprocess,json
 from flask import Flask, render_template, request, redirect, Response
 from ..Module.Predictor import onnx_model
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/process', methods=['POST'])
 def greet():
