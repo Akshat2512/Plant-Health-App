86698d2676814242dbb669af1032c4790a660b45
Akshat2512 <akshat.2016603@gmail.com>
2024-10-07 11:40:24 +0530

    deploy

--- api/index.py
+++ api/index.py
@@ -1,22 +1,29 @@
 import subprocess,json
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, Response, send_file
 from Module.Predictor import onnx_model
-
+from Module.process import process_image
 # from flask_cors import CORS
 
 app = Flask(__name__)
 # CORS(app)
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
-def greet():
+def process():
     req_data = request.json
     data = req_data['img']
     response = onnx_model(data)
     return Response(response, mimetype='application/json')
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route('/process_image', methods=['POST'])
+def image_proccessing():
+    data = request.json['file']
+    response = process_image(data)
+    print(response)
+    return send_file(response, mimetype='image/jpeg')
+
+if __name__ == "__main__":
+    app.run(debug=True)
